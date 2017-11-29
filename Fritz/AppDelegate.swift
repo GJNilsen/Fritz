03901e5f25532e00e33953d545c1085b108d4b28
GJ Nilsen <gj.nilsen@appfact.no>
2017-11-29 19:15:50 +0100

    Initial Commit

---
+++ Fritz/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Fritz
+//
+//  Created by Geert-Jan Nilsen on 29/11/2017.
+//  Copyright © 2017 Geert-Jan Nilsen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Fritz/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Fritz
+//
+//  Created by Geert-Jan Nilsen on 29/11/2017.
+//  Copyright © 2017 Geert-Jan Nilsen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
